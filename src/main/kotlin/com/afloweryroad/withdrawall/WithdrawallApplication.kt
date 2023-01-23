60236fee2a4195b761573ac6f9d93f95e8077281
LimHyuk <skdltm117@gmail.com>
2023-01-24 02:36:44 +0900

    Feat: Springboot initialization

---
+++ src/main/kotlin/com/afloweryroad/withdrawall/WithdrawallApplication.kt
@@ -0,0 +1,11 @@
+package com.afloweryroad.withdrawall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WithdrawallApplication
+
+fun main(args: Array<String>) {
+	runApplication<WithdrawallApplication>(*args)
+}
---
+++ src/test/kotlin/com/afloweryroad/withdrawall/WithdrawallApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.afloweryroad.withdrawall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WithdrawallApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
